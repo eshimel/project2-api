e068e273974a66c887aca0f85c8dbf774b31f2c2
EShimel <edieshimel@gmail.com>
2015-11-27 12:54:57 -0500

    successfully added new table and schemas...operation frankenstein has begun

---
+++ db/migrate/20151127175040_entries.rb
@@ -0,0 +1,11 @@
+class Entries < ActiveRecord::Migration
+   def change
+    create_table :entries do |t|
+      t.string :title
+      t.string :jpost
+      t.references :user
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806195336) do
+ActiveRecord::Schema.define(version: 20151127175040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "books", force: :cascade do |t|
+  create_table "entries", force: :cascade do |t|
     t.string   "title"
-    t.string   "isbn"
+    t.string   "jpost"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
